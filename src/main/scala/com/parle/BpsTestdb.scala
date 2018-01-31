3279a8bccc2c6bf97eec7d528db773056674438d
DarshanaGautam <darshana.gautam@antuit.com>
2018-01-31 18:00:31 +0530

    removed location

--- src/main/scala/com/parle/BpsTestdb.scala
+++ src/main/scala/com/parle/BpsTestdb.scala
@@ -135,7 +135,7 @@ object BpsTestdb {
 					import spark.implicits._
           import spark.sql
           
-					createQuery += columnString.substring(0,columnString.length()-1 ) + ") stored as TEXTFILE LOCATION '/user/darshana/'";
+					createQuery += columnString.substring(0,columnString.length()-1 ) + ") stored as TEXTFILE ";
 				  
 					println(createQuery)
 		
